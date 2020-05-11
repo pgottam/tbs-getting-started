c788b99ceedbfb32cab0981adafc7bef04598064
Jason Morgan <jmorgan@f9vs.com>
2020-05-11 14:52:08 -0400

    format updates.

--- tab1.sh
+++ tab1.sh
@@ -1,55 +1,68 @@
 #!/bin/bash
 . demo-magic.sh
 clear
-alias k=kubectl
 ## Assumes TBS is installed and ready to be configured
 
 pe "pb stack status"
 pe "kubectl get stack --all-namespaces"
+wait
+clear
+
 pe "pb store list"
 wait
 clear
 pe "kubectl get store --all-namespaces"
+wait
+clear
+
 pe "pb builder list"
 pe "kubectl get CustomClusterBuilder,CustomBuilder --all-namespaces"
+wait
+clear
 
 pe "pb project create 59s-prod"
 pe "pb project list"
 pe "kubectl get project"
 pe "pb project target 59s-prod"
-pe "pb secrets registry apply -f .priv/reg-creds.yaml"
-clear
-pe "cat image.yaml"
 wait
 clear
+
+pe "pb secrets registry apply -f .priv/reg-creds.yaml"
+pe "vim registry-creds.yaml"
+clear
+
+pe "vim image.yaml"
+wait
+clear
+
 pe "pb image apply -f image.yaml"
 
 ### Doesn't stream output Need to fix
 pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 1 -f"
-
-clear
 wait
+clear
+
 pe "pb image build index.docker.io/jasonmorgan/pbpetclinic -b 1"
-
-clear
 wait
+clear
+
 
 pe "docker pull jasonmorgan/pbpetclinic"
 pe "docker run -it --rm -p 8080:8080 jasonmorgan/pbpetclinic"
 pe "docker inspect jasonmorgan/pbpetclinic | less"
-
-clear
 wait
+clear
+
 
 pe "docker inspect jasonmorgan/pbpetclinic | jq '.[].Config.Labels.\"io.buildpacks.project.metadata\" | fromjson'"
-
-clear
 wait
+clear
+
 
 pe "docker inspect jasonmorgan/pbpetclinic | jq '.[].Config.Labels.\"io.buildpacks.lifecycle.metadata\" | fromjson | .buildpacks'"
-
-clear
 wait
+clear
+
 
 ## New tab
 
@@ -77,17 +90,8 @@ pe "docker run -it --rm -p 8080:8080 jasonmorgan/pbpetclinic"
 pe "pb stack update --build-image registry.pivotal.io/tbs-dependencies/build:1586272925 --run-image registry.pivotal.io/tbs-dependencies/run:1586272925"
 pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 3 -f"
 
-### Old stuff
+### Trigger Build
 
-## pb image trigger index.docker.io/jasonmorgan/pbpetclinic:latest
-## pb image logs index.docker.io/jasonmorgan/pbpetclinic:latest -b 4 -f
-## docker pull jasonmorgan/pbpetclinic
-## docker inspect jasonmorgan/pbpetclinic | less
-
-## jq examples
-## docker inspect jasonmorgan/pbpetclinic | jq '.[].Config.Labels."io.buildpacks.lifecycle.metadata" | fromjson'
-
-## docker inspect jasonmorgan/pbpetclinic | jq '.[].Config.Labels."io.buildpacks.lifecycle.metadata" | fromjson | .buildpacks'
-
-## Run 
+pe "pb image trigger index.docker.io/jasonmorgan/pbpetclinic:latest"
+pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic:latest -b 4 -f"
   
