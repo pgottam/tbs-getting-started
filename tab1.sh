7330b72881d05eb3764b4e7f6f09890828248c72
Jason Morgan <jmorgan@f9vs.com>
2020-05-13 11:47:27 -0400

    Fixing tab1.

--- tab1.sh
+++ tab1.sh
@@ -3,6 +3,7 @@
 clear
 ## Assumes TBS is installed and ready to be configured
 
+p "Let's take a tour around the TBS cli and objects"
 pe "pb stack status"
 pe "kubectl get stack --all-namespaces"
 wait
@@ -20,12 +21,17 @@ pe "kubectl get CustomClusterBuilder,CustomBuilder --all-namespaces"
 wait
 clear
 
-pe "pb project create 59s-prod"
+p "pb project create 59s-prod"
+pb project create 59s-prod > /dev/null 2>&1
+cat create-project-output.txt
+echo ""
 pe "pb project list"
 pe "kubectl get project"
 pe "pb project target 59s-prod"
 wait
 clear
+
+p "now that we have our project we can start on creating an image"
 
 pe "pb secrets registry apply -f .priv/reg-creds.yaml"
 pe "vim registry-creds.yaml"
@@ -49,6 +55,51 @@ clear
 
 pe "docker pull jasonmorgan/pbpetclinic"
 pe "docker run -it --rm -p 8080:8080 jasonmorgan/pbpetclinic"
+wait
+clear
+
+
+
+## New tab
+
+## Change to tonka in other tab
+p "Let's modify pet clinic"
+
+## Commit
+
+pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 2 -f"
+wait
+clear
+cmd
+## New tab
+
+pe "docker pull jasonmorgan/pbpetclinic"
+pe "docker run -it --rm -p 8080:8080 jasonmorgan/pbpetclinic"
+
+wait
+clear
+## STACK Update
+
+## Go find the latest run and build images
+### Browse to https://registry.pivotal.io
+### Go pull the latest image for registry.pivotal.io/tbs-dependencies/run and registry.pivotal.io/tbs-dependencies/build
+#### Ex:
+# docker pull registry.pivotal.io/tbs-dependencies/run:1586272925
+# docker pull registry.pivotal.io/tbs-dependencies/build:1586272925
+
+p "Now that we've done an update to the code let's see what updating the base image looks like"
+pe "docker login  registry.pivotal.io"
+pe "pb stack update --build-image registry.pivotal.io/tbs-dependencies/build:1586272925 --run-image registry.pivotal.io/tbs-dependencies/run:1586272925"
+pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 3 -f"
+cmd
+### Trigger Build
+
+p "We can also manually trigger a build at anytime"
+pe "pb image trigger index.docker.io/jasonmorgan/pbpetclinic:latest"
+pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic:latest -b 4 -f"
+cmd
+
+p "Here we can dive into the additional data provided by Cloud Native Buildpacks and TBS"
 pe "docker inspect jasonmorgan/pbpetclinic | less"
 wait
 clear
@@ -62,36 +113,3 @@ clear
 pe "docker inspect jasonmorgan/pbpetclinic | jq '.[].Config.Labels.\"io.buildpacks.lifecycle.metadata\" | fromjson | .buildpacks'"
 wait
 clear
-
-
-## New tab
-
-## Change to tonka in other tab
-
-## Commit
-
-pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 2 -f"
-
-## New tab
-
-pe "docker pull jasonmorgan/pbpetclinic"
-pe "docker run -it --rm -p 8080:8080 jasonmorgan/pbpetclinic"
-
-## STACK Update
-
-## Go find the latest run and build images
-### Browse to https://registry.pivotal.io
-### Go pull the latest image for registry.pivotal.io/tbs-dependencies/run and registry.pivotal.io/tbs-dependencies/build
-#### Ex:
-# docker pull registry.pivotal.io/tbs-dependencies/run:1586272925
-# docker pull registry.pivotal.io/tbs-dependencies/build:1586272925
-
-
-pe "pb stack update --build-image registry.pivotal.io/tbs-dependencies/build:1586272925 --run-image registry.pivotal.io/tbs-dependencies/run:1586272925"
-pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic -b 3 -f"
-
-### Trigger Build
-
-pe "pb image trigger index.docker.io/jasonmorgan/pbpetclinic:latest"
-pe "pb image logs index.docker.io/jasonmorgan/pbpetclinic:latest -b 4 -f"
-  
