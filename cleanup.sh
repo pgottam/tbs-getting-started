730a7599c1031d3f12bd7fe8d9364ac063e3bffa
JasonMorgan <jmorgan@f9vs.com>
2020-09-09 08:51:29 -0400

    Updated uninstall.

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 
-duffle uninstall 59s-build-service -c /tmp/credentials.yml -m /tmp/relocated.json
+ytt -f /tmp/values.yaml \
+    -f /tmp/manifests/ \
+    -v docker_repository="jasonmorgan" \
+    -v docker_username="jasonmorgan" \
+    -v docker_password='Ja5!T3#jWO9R' \
+    | kbld -f /tmp/images-relocated.lock -f- \
+    | kapp delete -a tanzu-build-service -y
+
 kubens default
